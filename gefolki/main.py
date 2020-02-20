72605dbe9a1f71832295faad10a4d06816b5d106
Fabrice Brito <fabrice.brito@terradue.com>
2020-02-20 10:31:12 +0100

    Update main.py

--- gefolki/main.py
+++ gefolki/main.py
@@ -7,8 +7,8 @@ from scipy.ndimage import imread
 
 import pylab as pl
 
-from algorithm import GEFolki, EFolki, Folki
-from tools import wrapData
+from .algorithm import GEFolki, EFolki, Folki
+from .tools import wrapData
 
 from PIL import Image
 
