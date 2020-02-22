740a250c7c8c66f9413e66381949d8aa2060af3f
Fabrice Brito <fabrice.brito@terradue.com>
2020-02-22 13:41:37 +0100

    Update __init__.py

--- gefolki/__init__.py
+++ gefolki/__init__.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import
 from .algorithm import GEFolki, EFolki, Folki
 from .tools import wrapData
 
-from .mining import mining
-from .main import demo
+#from .mining import mining
+#from .main import demo
 
 from .rank import rank_inf as rank_filter_inf
 from .rank import rank_sup as rank_filter_sup
