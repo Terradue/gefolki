984c73420a1f02fc1c5e653084a309a0dc03f231
pmembari <parham.membari@terradue.com>
2026-06-05 15:04:19 +0200

    add click, and loguru; and blacking

--- gefolki/algorithm.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from .folki import GEFolkiIter, EFolkiIter, FolkiIter
-from .pyramid import BurtOF
-
-GEFolki = BurtOF(GEFolkiIter)
-EFolki = BurtOF(EFolkiIter)
-Folki = BurtOF(FolkiIter)
